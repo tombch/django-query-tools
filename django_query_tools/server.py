126cbe63a2635dac1da4c095e34dd07ef6f9ee78
tombch <t.brier@outlook.com>
2023-05-15 12:33:20 +0100

    Added optional function to validate with a filterset

--- django_query_tools/server.py
+++ django_query_tools/server.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import FieldDoesNotExist, ValidationError
 from django.db.models import Q
 import operator
 import functools
@@ -21,7 +22,7 @@ def make_atoms(data, to_str=True):
     if len(data.items()) != 1:
         raise Exception
 
-    key, value = next(iter(data.items()))
+    key, value = next(iter(data.items()))  #  type: ignore
 
     if key in {"&", "|", "^"}:
         atoms = [make_atoms(k_v) for k_v in value]
@@ -47,6 +48,68 @@ def make_atoms(data, to_str=True):
         return [atom]
 
 
+def validate_atoms(
+    atoms, filterset, filterset_args=None, filterset_kwargs=None, filterset_model=None
+):
+    """
+    Use the provided `filterset` to validate and clean the provided list of `atoms`.
+    """
+    # Construct a list of dictionaries from the atoms
+    # Each of these dictionaries will be passed to a filterset
+    # The filterset is being used to clean and validate the input filters
+    # Until we construct the query, it doesn't matter how fields are related in the query (i.e. AND, OR, etc)
+    # All that matters is if the individual filters and their values are valid
+    layers = [{}]
+
+    for atom in atoms:
+        # Place the QueryAtom in the first dictionary where the key is not present
+        # If we reach the end with no placement, create a new dictionary and add it in there
+        for layer in layers:
+            if atom.key not in layer:
+                layer[atom.key] = atom
+                break
+        else:
+            layers.append({atom.key: atom})
+
+    if not filterset_args:
+        filterset_args = ()
+
+    if not filterset_kwargs:
+        filterset_kwargs = {}
+
+    if not filterset_model:
+        filterset_model = filterset.Meta.model
+
+    # Use a filterset, applied to each dict in layers, to validate the data
+    for i, layer in enumerate(layers):
+        # Slightly cursed, but it works
+        fs = filterset(
+            *filterset_args,
+            data={k: v.value for k, v in layer.items()},
+            queryset=filterset_model.objects.none(),
+            **filterset_kwargs,
+        )
+
+        # If unknown field lookups were provided, raise an exception
+        if i == 0:
+            unknown = []
+
+            for field in layer:
+                if field not in fs.filters:
+                    unknown.append(field)
+
+            if unknown:
+                raise FieldDoesNotExist(unknown)
+
+        # If not valid, return errors
+        if not fs.is_valid():
+            raise ValidationError(fs.errors)
+
+        # Add the cleaned values to the QueryAtom objects
+        for k, atom in layer.items():
+            atom.value = fs.form.cleaned_data[k]
+
+
 def make_query(data):
     """
     Traverses the provided `data` and forms the corresponding Q object.
@@ -54,7 +117,7 @@ def make_query(data):
     if len(data.items()) != 1:
         raise Exception
 
-    key, value = next(iter(data.items()))
+    key, value = next(iter(data.items()))  #  type: ignore
 
     # AND of multiple keyvalues
     if key == "&":
